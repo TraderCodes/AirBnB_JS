a018d3ed37f76309c7f4d4d46f7e26c6714cf675
jamal <TraderCodes@users.noreply.github.com>
2022-12-27 02:50:12 -0800

    clean

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 
 const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
+// const { requireAuth } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
