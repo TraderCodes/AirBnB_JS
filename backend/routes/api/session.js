58bc7fbf2dace33d83bafd2628de2a6a9f4af95e
jamal <TraderCodes@users.noreply.github.com>
2022-12-28 17:22:04 -0800

    Add get current userSession data routes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,6 +5,15 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+router.get('/', restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject(),
+    });
+  } else return res.json({ user: null });
+});
+
 router.post('/', async (req, res, next) => {
   const { credential, password } = req.body;
 
