2908c0b4b0d2253ec125d235400f842bbb144e06
jamal <TraderCodes@users.noreply.github.com>
2022-12-28 17:18:53 -0800

    sign up routes for users.js

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+
+
+// sign up
+router.post('/', async (req, res) => {
+  const { email, password, username ,firstName, lastName } = req.body;
+  const user = await User.signup({ email, username, password , firstName, lastName});
+
+  await setTokenCookie(res, user);
+
+  return res.json({
+    user: user,
+  });
+});
 module.exports = router;
+
+//
+// R4EdfFE7-xrBz7d4cZmqrVCOaVODIH5JMjZQ
+// 1
