ef70ce2c0e3d250261cdee6293550a9a35ae8772
jamal <TraderCodes@users.noreply.github.com>
2022-12-31 09:34:40 -0800

    Add Get all spots routes

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+const {
+  Review,
+  Spot,
+  ReviewImage,
+  User,
+  SpotImage,
+  sequelize,
+} = require('../../db/models');
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require('../../utils/auth');
+const { Op } = require('sequelize');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const spot = require('../../db/models/spot');
+
+const validateSpotGetAll = [
+  check('minLat')
+    .optional()
+    .isDecimal()
+    .withMessage('Minimum latitude is invalid'),
+  check('maxLat')
+    .optional()
+    .isDecimal()
+    .withMessage('Maximum latitude is invalid'),
+  check('minLng')
+    .optional()
+    .isDecimal()
+    .withMessage('Minimum longitude is invalid'),
+  check('maxLng')
+    .optional()
+    .isDecimal()
+    .withMessage('Maximum longitude is invalid'),
+  check('minPrice')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('Minimum price must be greater than or equal to 0'),
+  check('maxPrice')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('Maximum price must be greater than or equal to 0'),
+  handleValidationErrors,
+];
+router.get('/', validateSpotGetAll, async (req, res, next) => {
+  const getallSpots = await Spot.findAll({
+    include: [
+      {
+        model: Review,
+      },
+      {
+        model: SpotImage,
+      },
+    ],
+
+  });
+
+  const Spots = [];
+  getallSpots.forEach((spot) => {
+    Spots.push(spot.toJSON());
+  });
+
+  Spots.forEach((spot) => {
+    spot.SpotImages.forEach((spotImage) => {
+
+      if (spotImage.url) {
+        spot.previewImage = spotImage.url;
+      }
+    });
+    let count = 0;
+    let i = 0;
+    spot.Reviews.forEach((review) => {
+      i++;
+      count = count + review.stars;
+    });
+    spot.avgRating = count / i;
+    delete spot.SpotImages;
+    delete spot.Reviews;
+  });
+  res.json({
+    Spots,
+  });
+});
+module.exports = router;
