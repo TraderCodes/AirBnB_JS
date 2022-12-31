1a1ce22846bd777ee46019e8e39c8e14aa2aaf6d
jamal <TraderCodes@users.noreply.github.com>
2022-12-31 08:00:49 -0800

    Added signup routes / validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,33 +4,57 @@ const router = express.Router();
 const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { handleValidationErrors } = require('../../utils/validation');
+const {
+  handleValidationErrors,
+  handleDuplicateErrors,
+} = require('../../utils/validation');
 // validation middleware for Sign up
 const validateSignup = [
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
+    .withMessage('Invalid email.'),
   check('username')
     .exists({ checkFalsy: true })
-    .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
-  check('username').not().isEmail().withMessage('Username cannot be an email.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
+    // .isLength({ min: 4 })
+    .withMessage('Username is required.'),
+  check('username').not().isEmail().withMessage('Username is required'),
+  // check('password')
+  //   .exists({ checkFalsy: true })
+  //   .isLength({ min: 6 })
+  //   .withMessage('Password must be 6 characters or more.'),
   check('firstName')
     .exists({ checkFalsy: true })
-    .withMessage('Must Provide a first name'),
+    .withMessage('First Name is required'),
   check('lastName')
     .exists({ checkFalsy: true })
-    .withMessage('Must Provide a last name'),
+    .withMessage('Last Name is required'),
   handleValidationErrors,
 ];
-// sign up
-router.post('/', validateSignup, async (req, res) => {
+
+router.post('/' , validateSignup, async (req, res) => {
   const { email, password, username, firstName, lastName } = req.body;
+  // console.log(req.body.email)
+  const allUsers = await User.findAll();
+  for (let user of allUsers) {
+    // console.log(user.dataValues)
+    if (email === user.dataValues.email) {
+        res.status(403);
+        return res.json({
+          message: 'User already exists',
+          statusCode: 403,
+          errors: ['User with that email already exists'],
+        });
+      }
+    if (username === user.dataValues.username) {
+      res.status(403);
+      return res.json({
+        message: 'User already exists',
+        statusCode: 403,
+        errors: ['User with that username already exists'],
+      });
+    }
+  }
   const user = await User.signup({
     email,
     username,
@@ -38,11 +62,17 @@ router.post('/', validateSignup, async (req, res) => {
     firstName,
     lastName,
   });
-
   await setTokenCookie(res, user);
-
+  let token = await setTokenCookie(res, user);
   return res.json({
-    user: user,
+    user: {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      username: user.username,
+      token: token,
+    },
   });
 });
 module.exports = router;
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -8,9 +8,24 @@ const handleValidationErrors = (req, _res, next) => {
   if (!validationErrors.isEmpty()) {
     const errors = validationErrors.array().map((error) => `${error.msg}`);
 
-    const err = Error('Bad request.');
+    const err = Error('Validation error');
     err.errors = errors;
     err.status = 400;
+    
+    err.title = 'Bad request.';
+    next(err);
+  }
+  next();
+};
+const handleDuplicateErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors.array().map((error) => `${error.msg}`);
+
+    const err = Error('Validation error');
+    err.errors = errors;
+    err.status = 403;
     err.title = 'Bad request.';
     next(err);
   }
@@ -19,4 +34,5 @@ const handleValidationErrors = (req, _res, next) => {
 
 module.exports = {
   handleValidationErrors,
+  handleDuplicateErrors,
 };
