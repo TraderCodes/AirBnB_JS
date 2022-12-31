ca6a161b11700d0433472aebf4d01b0a4da3a484
jamal <TraderCodes@users.noreply.github.com>
2022-12-31 02:36:04 -0800

    Added booking model

---
+++ backend/db/models/booking.js
@@ -0,0 +1,19 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+
+    static associate(models) {
+
+    }
+  }
+  Booking.init({
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Booking',
+  });
+  return Booking;
+};
