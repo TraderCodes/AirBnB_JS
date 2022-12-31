f6dba1b3f7787b8514acb7f2119d2a978ce08d4e
jamal <TraderCodes@users.noreply.github.com>
2022-12-31 01:59:31 -0800

    Added spotImage model

---
+++ backend/db/migrations/20221231095534-create-spot-image.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'SpotImages',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: 'Spots' },
+        },
+        url: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        preview: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('Current_TimeStamp'),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('Current_TimeStamp'),
+        },
+      },
+      options
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'SpotImages';
+    await queryInterface.dropTable(options);
+  },
+};
