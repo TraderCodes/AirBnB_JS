52b3f3a0754f2cb33ffae7f93e47f77238753ca4
jamal <TraderCodes@users.noreply.github.com>
2023-01-17 02:55:07 -0800

    Added navigation / login / profile button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
 import * as sessionActions from './store/session';
-
+import Navigation from './components/Navigation';
 function App() {
     const dispatch = useDispatch();
     const [isLoaded, setIsLoaded] = useState(false);
@@ -13,17 +13,17 @@ function App() {
     }, [dispatch]);
   return (
     <>
-      <Switch>
-        {/* <Route path='/'>
-          <h2>/login to log in </h2>
-        </Route> */}
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
     </>
   );
 }
