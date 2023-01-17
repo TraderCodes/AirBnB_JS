db59a06a50d6cfb2b5469591780274fdf933cf2b
jamal <TraderCodes@users.noreply.github.com>
2023-01-17 03:11:20 -0800

    Added closing the dropdown menu on any click

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 
@@ -6,16 +6,31 @@ function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
 
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
+  useEffect(() => {
+      if (!showMenu) return;
+    const closeMenu = (e) => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
 
   const ulClassName = 'profile-dropdown' + (showMenu ? '' : ' hidden');
 
   return (
     <>
-      <button onClick={() => setShowMenu(!showMenu)}>
+      <button onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
       <ul className={ulClassName}>
